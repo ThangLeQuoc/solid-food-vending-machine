c5bbd832a82dbcac27d308e720439684e76de935
Thang Le Quoc <silverhair.guy@gmail.com>
2018-06-08 19:53:57 +0700

    Implement PriceTag

--- src/com/greenduck/vendingmachine/foods/PriceTag.java
+++ src/com/greenduck/vendingmachine/foods/PriceTag.java
@@ -1,5 +1,22 @@
 package com.greenduck.vendingmachine.foods;
 
+import com.greenduck.vendingmachine.money.Currency;
+
+//TODO: add java doc
 public class PriceTag {
+    private double price;
+    private Currency currency;
     
+    public PriceTag(Currency currency, double price) {
+        this.currency = currency;
+        this.price = price;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public Currency getCurrency() {
+        return currency;
+    }
 }
