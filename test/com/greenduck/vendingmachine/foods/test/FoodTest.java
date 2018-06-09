63902c4fbd97a9fc862096bc7f365a926a22ebf8
Thang Le Quoc <silverhair.guy@gmail.com>
2018-06-09 15:28:55 +0700

    Add test for concrete foods
    - Add test for concrete food

--- test/com/greenduck/vendingmachine/foods/test/FoodTest.java
+++ test/com/greenduck/vendingmachine/foods/test/FoodTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 
 import com.greenduck.vendingmachine.common.test.FoodVendingTestConstant;
 import com.greenduck.vendingmachine.foods.Biscuit;
+import com.greenduck.vendingmachine.foods.ChocolateBar;
 import com.greenduck.vendingmachine.foods.Food;
+import com.greenduck.vendingmachine.foods.InstantNoodle;
+import com.greenduck.vendingmachine.foods.Sausage;
 import com.greenduck.vendingmachine.money.Currency;
 import com.greenduck.vendingmachine.money.PriceTag;
 
@@ -25,5 +28,41 @@ public class FoodTest {
         assertEquals(Currency.USD, food.getPriceTag().getCurrency());
         assertEquals(5.60, food.getPriceTag().getPrice(), FoodVendingTestConstant.EPSILON);
         
+    }
+    
+    @Test
+    public void testChocolateBarCreation_ShouldReturnCorrectProperties() {
+        
+        food = new ChocolateBar();
+        double price = 2.23;
+        PriceTag priceTag = new PriceTag(Currency.CHF, price);
+        food.setPriceTag(priceTag);
+        assertEquals(ChocolateBar.NAME, food.getName());
+        assertEquals(Currency.CHF, food.getPriceTag().getCurrency());
+        assertEquals(price, food.getPriceTag().getPrice(), FoodVendingTestConstant.EPSILON);
+    }
+    
+    @Test
+    public void testInstantNoodleCreation_ShouldReturnCorrectProperties() {
+        food = new InstantNoodle();
+        double price = 3.40;
+        PriceTag priceTag = new PriceTag(Currency.EUR, price);
+        food.setPriceTag(priceTag);
+        
+        assertEquals(InstantNoodle.NAME, food.getName());
+        assertEquals(Currency.EUR, food.getPriceTag().getCurrency());
+        assertEquals(price, food.getPriceTag().getPrice(), FoodVendingTestConstant.EPSILON);
+    }
+    
+    @Test
+    public void testSausageCreation_ShouldReturnCorrectProperties() {
+        food = new Sausage();
+        double price = 15000;
+        PriceTag priceTag = new PriceTag(Currency.VND, price);
+        food.setPriceTag(priceTag);
+        
+        assertEquals(Sausage.NAME, food.getName());
+        assertEquals(Currency.VND, food.getPriceTag().getCurrency());
+        assertEquals(price, food.getPriceTag().getPrice(), FoodVendingTestConstant.EPSILON);
     }
 }
